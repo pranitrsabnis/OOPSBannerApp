18f683eca1586b0c8e0172f5b9556be7a7bde4b3
pranitrsabnis <pranitrsabnis@gmail.com>
2026-02-24 15:49:18 +0530

    Create OOPSBannerApp.java

---
+++ OOPSBannerApp.java
@@ -0,0 +1,15 @@
+/**
+ * OOPSBannerApp UC1- OOPS Banner Display Application
+ *
+ * This class demostrates a simple Java application that displays the object
+ * Oreinted Programming system OOPS acronym to the console 
+ *
+ * @author Developer
+ * @version 1.0
+ */
+ 
+ public class OOPSBannerApp {
+	 public static void main(String[] args) {
+		 System.out.println(x:"OOPS");
+	 }
+ }
